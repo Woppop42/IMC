aa7704289fd5b813f5462df098e933cadbddb4d2
Gabriel <gokba27@gmail.com>
2023-05-15 16:51:56 +0200

    Modif de la fonction de calcul

--- script.js
+++ script.js
@@ -7,5 +7,38 @@ function calculImc(event)
     result = document.querySelector("#result");
     result.textContent = "Vous avez un IMC de " + imc;
     return imc;
-}   
+}  
 
+
+function handleForm()
+{
+    let taille = document.querySelector('#taille').value;
+    let poids = document.querySelector('#poids').value;
+    console.log(taille, poids);
+    return [poids, taille];
+} 
+
+
+function calcul(taille, poids)
+{
+    let imc = poids / ((taille/100) * (taille/ 100));
+    console.log(imc);
+    return imc;
+}
+
+
+function displayResult(imc)
+{
+    let result = document.querySelector("#result");
+    result.textContent = "Vous avez un IMC de " + imc;
+    
+}
+
+function finale(event)
+{
+    let taille = document.querySelector('#taille').value;
+    let poids = document.querySelector('#poids').value;
+    event.preventDefault();
+    imc = calcul(taille, poids);
+    displayResult(imc);
+}
