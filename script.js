b51d0aa885fb65e5eebed560b60a3ff25aa8f1bd
Gabriel <gokba27@gmail.com>
2023-05-16 18:08:13 +0200

    Ajout description

--- script.js
+++ script.js
@@ -33,6 +33,37 @@ function displayResult(imc)
     result.textContent = "Vous avez un IMC de " + imc;
     
 }
+function displayDescription(imc)
+{
+    let description;
+    if(imc < 18.5)
+    {
+         description = "La maigreur pathologique augmente le risque de maladies  et infections diverses. Elle peut également être non pathologique, c'est-à-dire constitutionnelle, stable et ne s'accompagnant d'aucun trouble.";
+        
+    } else if(imc > 18.5 && imc < 25)
+    {
+        description = "Corpulence normale, bornée par la maigreur et le surpoids.";
+        
+    }else if( imc >= 25 && imc < 30)
+    {
+        description = "Chez l'humain, le surpoids est défini par l'Organisation mondiale de la santé comme une accumulation anormale ou excessive de graisse qui présente un risque pour la santé.";
+        
+    }else if(imc >= 30 && imc < 35)
+    {
+        description = "L'obésité légère expose à 70% de risques supplémentaires d'insuffisance cardiaque.";
+        
+    }else if(imc >= 35 && imc < 40)
+    {
+        description = "L'obésité sévère rend l'adulte 12 fois plus susceptibles de développer un diabète de type 2, et le rend 22 fois plus à risque d'apnée du sommeil (par rapport aux pairs de poids normal).";
+        
+    }else if(imc > 40)
+    {
+        description = "L'obésité morbide expose à des complications très sévères telles que problèmes cardiovasculaires, insuffisance respiratoire avec hypoventilation alvéolaire ou encore cancer hépatique, pouvant aller jusqu'au décès.";
+        
+    }
+    let descText = document.querySelector('#description');
+    descText.textContent = description;
+}
 
 function finale(event)
 {
@@ -41,4 +72,5 @@ function finale(event)
     event.preventDefault();
     imc = calcul(taille, poids);
     displayResult(imc);
+    displayDescription(imc);
 }
